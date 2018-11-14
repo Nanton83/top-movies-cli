a353b1a347c1606a2055a232833385c4b3b6396b
Nick Romans <nicholas.romans@gmail.com>
2018-11-14 02:06:07 +0000

    added scraper

---
+++ lib/top_movies_cli/scraper.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+
+class FindAPark::Scraper
+
+  def self.scraped_data
+    doc = Nokogiri::HTML(open("https://www.afi.com/100Years/movies.aspx"))
+    doc
+  end
+
+  def self.movie_title
+    @titles = self.scraped_data.css('.filmTitle').text.split("\n")
+    @titles.map do |title|
+      movie = title.strip
+      movie
+    end
+  end
+
+end
