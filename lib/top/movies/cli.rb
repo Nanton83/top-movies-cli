8b2b26729cdf61ce13310cdba50526b3659c7758
Nick Romans <nicholas.romans@gmail.com>
2018-11-14 01:54:27 +0000

    first commit

---
+++ lib/top/movies/cli.rb
@@ -0,0 +1,10 @@
+require "top/movies/cli/version"
+
+module Top
+  module Movies
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/top/movies/cli/version.rb
@@ -0,0 +1,7 @@
+module Top
+  module Movies
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
